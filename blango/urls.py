fed8964c632ebc14748c9dd6f64699f97e121204
ALEXIOS NIKOLOPOULOS <alexis.nikolopoulos@hotmail.com>
2022-07-05 17:46:28 +0000

    Finish DRF views

--- blango/urls.py
+++ blango/urls.py
@@ -39,7 +39,8 @@ urlpatterns = [
     name="django_registration_register",
 ),
     path("accounts/", include("django_registration.backends.activation.urls")),
-    path("api/v1/", include("blog.api_urls")),
+    # path("api/v1/", include("blog.api_urls")),
+    path("api/v1/", include("blog.api.urls")),
     
 ]
 
